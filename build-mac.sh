60b856aeedf5324bd3c0497d3f2ac9713eda08c0
iffyloop <35245220+iffyloop@users.noreply.github.com>
2024-11-16 16:34:58 -0600

    Update build-mac.sh

--- build-mac.sh
+++ build-mac.sh
@@ -20,7 +20,7 @@ arch --x86_64 pip uninstall -y numpy
 arch --x86_64 pip install numpy==1.26.4
 arch --x86_64 pip install pyinstaller
 
-arch --x86_64 pyinstaller main.py --noupx --hidden-import tsr --hidden-import tsr.system --hidden-import tsr.utils --hidden-import tsr.models --hidden-import tsr.models.isosurface --hidden-import tsr.models.nerf_renderer --hidden-import tsr.models.network_utils --hidden-import tsr.models.tokenizers --hidden-import tsr.models.tokenizers.image --hidden-import tsr.models.tokenizers.triplane --hidden-import tsr.models.transformer --hidden-import tsr.models.transformer.attention --hidden-import tsr.models.transformer.basic_transformer_block --hidden-import tsr.models.transformer.transformer_1d --add-data "gui/build:gui_build" --add-data "checkpoints:checkpoints" --add-data "autoremesher:autoremesher"
+arch --x86_64 pyinstaller main.py --noupx --hidden-import tsr --hidden-import tsr.system --hidden-import tsr.utils --hidden-import tsr.models --hidden-import tsr.models.isosurface --hidden-import tsr.models.nerf_renderer --hidden-import tsr.models.network_utils --hidden-import tsr.models.tokenizers --hidden-import tsr.models.tokenizers.image --hidden-import tsr.models.tokenizers.triplane --hidden-import tsr.models.transformer --hidden-import tsr.models.transformer.attention --hidden-import tsr.models.transformer.basic_transformer_block --hidden-import tsr.models.transformer.transformer_1d --add-data "gui/build:gui_build" --add-data "autoremesher:autoremesher"
 cp -r venv/lib/python3.12/site-packages/vtkmodules/util dist/main/_internal/vtkmodules
 
 mv dist/main dist/meshfinity-release
